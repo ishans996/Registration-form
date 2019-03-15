d04904401266a96fd4e3e95b3c4b58b6be2ca3dc
ishans996 <ishans996@gmail.com>
2019-03-15 20:00:51 +0530

    webpage rendered

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template, flash
+from app import db, app
+from app.forms import RegistrationForm
+from app.models import User
+
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(_('Congratulations, you are now a registered user!'))
+    return render_template('register.html', title='Register',
+                           form=form)
